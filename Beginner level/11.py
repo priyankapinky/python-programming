e920c4256b1b527b625e12f3fd424bf1acc4e78b
RavichandranBE <37319294+RavichandranBE@users.noreply.github.com>
2018-03-27 15:15:35 +0530

    Create 11.py

---
+++ Beginner level/11.py
@@ -0,0 +1,6 @@
+a=str(input())
+    if(a=='SUNDAY' or a=='Sunday' or a=='sunday'or a=='SATURDAY' or a=='Saturday' or a=='saturday'):
+        print("Holiday")
+    else:
+        print("Working day")
+        p
